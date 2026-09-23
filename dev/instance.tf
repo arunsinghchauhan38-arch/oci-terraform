269ca59102e3d5f2cfe5917b3f41178a8d3a0ad2
arunsinghchauhan38-arch <arunsinghchauhan38@gmail.com>
2026-09-23 21:58:00 +0530

    Delete dev directory
    for security purpose

--- dev/instance.tf
+++
@@ -1,7 +0,0 @@
-module "dev_instance" {
-source = "/home/arunsinghc/instance_codes/instance_module"
-for_each = var.inst_var
-env_display_name = each.value.dev_display_name
-env_memory_in_gbs = each.value.dev_memory_in_gbs
-env_ocpus = each.value.dev_ocpus   
-}
