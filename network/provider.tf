4f1514a92f60bbacf7edf6126304fa3943bf7c87
arunsinghchauhan38-arch <arunsinghchauhan38@gmail.com>
2026-09-23 21:59:16 +0530

    Delete network directory

--- network/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "oci" {
-    auth                = "APIKey" 
-    tenancy_ocid        = "ocid1.tenancy.oc1..aaaaaaaae264lss2tpg5w3rc6ktkttm22tjyzy46tgfnwihjdaw7x5csbkea"
-    user_ocid           = "ocid1.user.oc1..aaaaaaaapqcjvc574ck55obqyviqetxyx4xtoazcio2hwvl3ux7a55wqcrqa"
-    fingerprint         = "95:fc:ce:5c:4e:2e:dd:80:10:67:03:57:20:47:28:a6"
-    private_key_path    = "/home/arunsinghc/compartment_template/priv.pem"
-    region              = "us-ashburn-1"
-}
